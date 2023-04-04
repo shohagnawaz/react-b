9e684fbf42860001f19a8f5f07cfeb693bf3b0db
shohagnawaz <snawazdigital@gmail.com>
2023-04-05 00:49:13 +0600

    Slider

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Fragment } from 'react';
 import Header from './components/Header/Header';
+import Slider from './components/Slider/Slider';
 
 function App() {
   return (
     <Fragment className="App">
       <Header />
+      <Slider />
     </Fragment>
   );
 }
